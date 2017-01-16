af81d4bc93807043d97bec3554f64df48e19d922
Ping-Ke Shih <pkshih@realtek.com>
2017-01-16 14:13:58 +0800

    rtlwifi: TX report uses 6 bits as sequence number.
    In new design, SW_DEFINE[1:0] are used by firmware, and
    the TX report only contains SW_DEFINE[7:0].
    Thus, driver can use SW_DEFINE[7:2] as sequence number.
    
    Besides, the format of tx report have been changed, so we add a flag
    RTL_SPEC_NEW_FW_C2H to access report.
    
    PS. We should merge this commit with previous one
    
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>

--- wifi.h
+++ wifi.h
@@ -961,6 +961,7 @@ enum package_type {
 enum rtl_spec_ver {
 	RTL_SPEC_NEW_RATEID = BIT(0),	/* use ratr_table_mode_new */
 	RTL_SPEC_SUPPORT_VHT = BIT(1),	/* support VHT */
+	RTL_SPEC_NEW_FW_C2H = BIT(2),	/* new FW C2H (e.g. TX REPORT) */
 };
 
 struct octet_string {
